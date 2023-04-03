81947f43d9512aa81f52bf6dd51bb90659cfed52
PTOM76 <ptms76@outlook.jp>
2023-04-03 15:24:26 +0900

    Fix redstone pipes made by Kneelawk

--- src/main/java/ml/pkom/pipeplus/pipe/PipeSpBehaviourRedstone.java
+++ src/main/java/ml/pkom/pipeplus/pipe/PipeSpBehaviourRedstone.java
@@ -1,30 +1,40 @@
 package ml.pkom.pipeplus.pipe;
 
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
 import alexiil.mc.mod.pipes.pipe.PartSpPipe;
 import alexiil.mc.mod.pipes.pipe.PipeSpBehaviour;
 import alexiil.mc.mod.pipes.pipe.PipeSpFlowItem;
-import ml.pkom.pipeplus.blocks.RedstonePipe;
-import net.minecraft.block.BlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
+import alexiil.mc.lib.multipart.api.property.MultipartProperties;
+import alexiil.mc.lib.multipart.api.property.MultipartPropertyContainer;
 
 public class PipeSpBehaviourRedstone extends PipeSpBehaviour {
 
     public PipeSpBehaviourRedstone(PartSpPipe pipe) {
         super(pipe);
     }
-    
+
     @Override
     public void tick() {
         super.tick();
         World world = pipe.getPipeWorld();
-        BlockPos pos = pipe.getPipePos();
         if (world == null) return;
         if (!world.isClient) {
-            RedstonePipe block = ((RedstonePipe) pipe.definition.pipeBlock);
-            BlockState state = world.getBlockState(pos);
-            block.setRedstoneSignal(state, world, pos, !((PipeSpFlowItem) pipe.getFlow()).getAllItemsForRender().isEmpty());
-            block.updatePoweredStatus(world, pos, state);
+            MultipartPropertyContainer properties = pipe.container.getProperties();
+
+            if (((PipeSpFlowItem) pipe.getFlow()).getAllItemsForRender().isEmpty()) {
+                properties.clearValue(pipe, MultipartProperties.getStrongRedstonePower(Direction.UP));
+                for (Direction dir : Direction.values()) {
+                    properties.clearValue(pipe, MultipartProperties.getWeakRedstonePower(dir));
+                }
+            } else {
+                properties.setValue(pipe, MultipartProperties.getStrongRedstonePower(Direction.UP), 15);
+                for (Direction dir : Direction.values()) {
+                    properties.setValue(pipe, MultipartProperties.getWeakRedstonePower(dir), 15);
+                }
+            }
         }
     }
 }
