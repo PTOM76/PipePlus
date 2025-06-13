711802dc86012918754c1d71efc7005baab58237
PTOM76 <ptms76@outlook.jp>
2025-06-13 23:58:15 +0900

    fix: #31 enhance fluid extraction direction checks in PipeSpBehaviourFluidExtract

--- src/main/java/net/pitan76/pipeplus/pipe/PipeSpBehaviourFluidExtract.java
+++ src/main/java/net/pitan76/pipeplus/pipe/PipeSpBehaviourFluidExtract.java
@@ -1,5 +1,6 @@
 package net.pitan76.pipeplus.pipe;
 
+import alexiil.mc.lib.attributes.fluid.impl.EmptyFluidExtractable;
 import alexiil.mc.mod.pipes.pipe.PartSpPipe;
 import alexiil.mc.mod.pipes.pipe.PipeSpFlowFluid;
 import net.minecraft.util.math.Direction;
@@ -10,7 +11,15 @@ public class PipeSpBehaviourFluidExtract extends PipeSpBehaviourItemExtract {
     }
 
     @Override
+    protected boolean canFaceDirection(Direction dir) {
+        if (pipe.getNeighbourPipe(dir) != null)
+            return false;
+
+        return pipe.getFluidExtractable(dir) != EmptyFluidExtractable.NULL;
+    }
+
+    @Override
     public void tryExtract(Direction dir, int pulses) {
-        ((PipeSpFlowFluid)pipe.getFlow()).tryExtract(dir);
+        ((PipeSpFlowFluid) pipe.getFlow()).tryExtract(dir);
     }
 }
